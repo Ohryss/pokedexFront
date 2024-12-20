1753e5caf3b06fac71887f2e19cc554aeaa8d5be
Ohryss <trochain.thomas@outlook.com>
2024-12-20 09:24:16 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {provideHttpClient, HttpClientModule} from "@angular/common/http";
+import {provideHttpClient} from "@angular/common/http";
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './shared/layouts/pokemon-card/pokemon-card.component';
 import { PokemonDetailComponent } from './pages/pokemon-detail/pokemon-detail.component';
 import { LangSelectorComponent } from './shared/layouts/lang-selector/lang-selector.component';
-import { TranslocoRootModule } from './transloco-root.module'; // Import du provider
+import { CommonModule } from '@angular/common';
+import { TranslocoRootModule } from './transloco-root.module';
 import { FormsModule } from '@angular/forms';
 import { LearnMoveComponent } from './shared/layouts/learn-move/learn-move.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SearchPokemonComponent } from './pages/search-pokemon/search-pokemon.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
+    ProfileComponent,
     AppComponent,
     PokemonListComponent,
     PokemonCardComponent,
     PokemonDetailComponent,
     LangSelectorComponent,
-    LearnMoveComponent
+    LearnMoveComponent,
+    LoginComponent,
+    SearchPokemonComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     TranslocoRootModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [
     provideHttpClient(), // Ajout du provider
