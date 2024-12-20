4425ddfcaf574b4c3b743ce176ee5d4bf6b63989
Ohryss <trochain.thomas@outlook.com>
2024-12-20 09:23:19 +0100

    Create moves.ts

---
+++ src/app/shared/interfaces/moves.ts
@@ -0,0 +1,11 @@
+export interface Moves {
+    id: number;
+    power: number;
+    accuracy: number;
+    pp: number;
+    move_damage_class_id: number;
+    priority: number;
+    type_id: number;
+    name: string;
+    description: string;
+}
