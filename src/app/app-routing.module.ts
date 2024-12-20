1542aff89ecbaafd5b99cd0ee8b3771421fb6125
Ohryss <trochain.thomas@outlook.com>
2024-12-20 09:21:10 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {PokemonListComponent} from "./pages/pokemon-list/pokemon-list.component";
-import {PokemonDetailComponent} from "./pages/pokemon-detail/pokemon-detail.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pages/pokemon-detail/pokemon-detail.component'
+import { AuthGuard } from './shared/guards/auth.guard';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'search-results', component: SearchResultsComponent },
   {
     path: '',
-    component: PokemonListComponent
+    component: PokemonListComponent,
+    canActivate: [AuthGuard]
   },
-
   {
     path: 'pokemon/:pokemon_id',
     component: PokemonDetailComponent
   }
 ];
 
+
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
